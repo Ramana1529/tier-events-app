4c1affcec2468bc97e72abb01dcc2ce97668d422
Ramana1529 <gvramana551@gmail.com>
2025-08-03 00:24:14 +0530

    Added sign-in, sign-up pages and routing

---
+++ app/middleware.js
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)", "/api(.*)"],
+});
+
+export const config = {
+  matcher: ["/((?!_next|.*\\..*).*)"], // match everything except static files
+};
